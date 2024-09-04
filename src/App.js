ba6d52ba76a35b14c07644de055e69c7beaf6aa2
Raza <razasfs2003@gmail.com>
2024-09-05 00:23:07 +0530

    testing 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>
+    <div style={{ textAlign: 'center' ,backgroundColor:"red"}}>
       appaskjdnkla
     </div>
   )
